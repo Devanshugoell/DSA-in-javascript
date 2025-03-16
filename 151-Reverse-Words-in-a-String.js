74616560de2352f6e97539dee2a351aa6a0d8ccd
Devanshu <devgoyal952@gmail.com>
2025-03-16 12:26:17 +0530

    added optimal approach 151-reverse-words-in-a-string.js

--- 151-Reverse-Words-in-a-String.js
+++ 151-Reverse-Words-in-a-String.js
@@ -14,4 +14,24 @@ var reverseWords = function (s) {
   return str.trim();
 };
 
+//This is more optimized version
+
+var reverseWords = function (s) {
+  let str = "";
+  let pointer = s.length;
+
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (s[i] === " " || i === 0) {
+      let start = i === 0 ? i : i + 1;
+      let word = s.substring(start, pointer).trim();
+      if (word) {
+        if (str.length > 0) str += " ";
+        str += word;
+      }
+      pointer = i;
+    }
+  }
+  return str;
+};
+
 console.log(reverseWords("a good   example"));
