893f57adf7a1c54db473ae24fcf5be400672a5e7
Devanshu <devgoyal952@gmail.com>
2025-03-24 00:05:25 +0530

    Added another method

--- 34-Find-First-and-Last-Position-of-Element-in-Sorted-Array.js
+++ 34-Find-First-and-Last-Position-of-Element-in-Sorted-Array.js
@@ -34,4 +34,37 @@ var searchRange = function (nums, target) {
   return [first, last];
 };
 
+var searchRange = function (nums, target) {
+  let result = [-1, -1];
+
+  // Find leftmost index
+  let left = 0,
+    right = nums.length - 1;
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1; // Even if nums[mid] === target, move left
+    }
+  }
+  // Check if target is found
+  if (nums[left] !== target) return result;
+  result[0] = left;
+
+  // Find rightmost index
+  right = nums.length - 1;
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (nums[mid] > target) {
+      right = mid - 1;
+    } else {
+      left = mid + 1; // Even if nums[mid] === target, move right
+    }
+  }
+  result[1] = right;
+
+  return result;
+};
+
 console.log(searchRange([5, 7, 7, 8, 8, 10], 8));
