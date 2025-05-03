8f9900ae8e4aff4f5681fb49d68cccd285600ba3
Devanshu <devgoyal952@gmail.com>
2025-05-03 14:15:04 +0530

    33-Search-in-Rotated-Sorted-Array.js

---
+++ 33-Search-in-Rotated-Sorted-Array.js
@@ -0,0 +1,29 @@
+var search = function (nums, target) {
+  let left = 0,
+    right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (target === nums[mid]) return mid;
+
+    //Then mid is in the left array of that rotated array
+    if (nums[left] <= nums[mid]) {
+      if (nums[left] <= target && target < nums[mid]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    } else {
+      if (target > nums[mid] && target <= nums[right]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+  }
+
+  return -1;
+};
+
+console.log(search([4, 5, 6, 7, 0, 1, 2], 0));
